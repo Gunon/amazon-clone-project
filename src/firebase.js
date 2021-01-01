445ab8b8e509728d9d596dbe629390b09bbda692
Daniel Gildardo Guerrero Guzman <gunon.96@gmail.com>
2021-01-01 13:54:47 -0600

    starter work

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBT8tx2USMzDxKMWv8jo9I-OP_b63N_boc",
+    authDomain: "clone-2c79a.firebaseapp.com",
+    projectId: "clone-2c79a",
+    storageBucket: "clone-2c79a.appspot.com",
+    messagingSenderId: "1066907808928",
+    appId: "1:1066907808928:web:a0c8ff2b76371e1cd045c5",
+    measurementId: "G-85QPQCKTEV"
+  };
