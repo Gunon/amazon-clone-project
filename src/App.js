965cbcb4a448c62a694e1728363a168323bfb9e7
Daniel Gildardo Guerrero Guzman <gunon.96@gmail.com>
2021-01-04 19:27:03 -0600

    added routing with react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import './App.css';
 import Header from './Header';
 import Home from './Home';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 function App() {
   return (
-    // BEM CONVENTION FOR STYLING
-    <div className="App">
-      <Header/>
-      <Home/>
-      {/* Home*/}
-    </div>
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route path="/checkout">
+            <h1>I AM A CHECKOUT</h1>
+          </Route>
+          <Route path="/">
+            <Header/>
+            <Home/>
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
